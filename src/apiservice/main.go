1f1969b434cb24d8e2a7cbd422a73f2f6a69f70c
Filippa Leuckfeld <filippaleuckfeld@Filippas-MacBook-Air.local>
2023-06-20 22:05:47 +0200

    Added communication with gRPC

--- src/apiservice/main.go
+++ src/apiservice/main.go
@@ -1,17 +1,43 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"time"
 
-	"api-service/handlers"
 	"github.com/gorilla/mux"
+	"github.com/pkg/errors"
+	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
+	"google.golang.org/grpc"
 )
 
+type apiServer struct {
+	productCatalogSvcAddr string
+	productCatalogSvcConn *grpc.ClientConn
+}
+
+func mustConnGRPC(ctx context.Context, conn **grpc.ClientConn, addr string) {
+	var err error
+	ctx, cancel := context.WithTimeout(ctx, time.Second*3)
+	defer cancel()
+	*conn, err = grpc.DialContext(ctx, addr,
+		grpc.WithInsecure(),
+		grpc.WithUnaryInterceptor(otelgrpc.UnaryClientInterceptor()),
+		grpc.WithStreamInterceptor(otelgrpc.StreamClientInterceptor()))
+	if err != nil {
+		panic(errors.Wrapf(err, "grpc: failed to connect %s", addr))
+	}
+}
+
 func main() {
+	ctx := context.Background()
+	svc := new(apiServer)
+	mustConnGRPC(ctx, &svc.productCatalogSvcConn, svc.productCatalogSvcAddr)
+
 	router := mux.NewRouter()
-	router.Handle("/products", handlers.GetProductHandler()).Methods("GET")
-	router.Handle("/products", handlers.CreateProductHandler()).Methods("POST")
+	router.HandleFunc("/products", svc.productsHandler).Methods(http.MethodGet, http.MethodHead)
+	//router.Handle("/products", handlers.CreateProductHandler()).Methods("POST")
 	server := http.Server{
 		Addr:    ":9090",
 		Handler: router,
